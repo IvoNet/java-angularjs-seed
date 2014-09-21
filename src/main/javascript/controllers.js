68ec9d1e4bdcbb20956c7feaa79d3cb274a9afc4
IvoNet <webmaster@ivonet.nl>
2014-09-21 21:53:53 +0200

    reformatted some code

--- src/main/javascript/controllers.js
+++ src/main/javascript/controllers.js
@@ -2,14 +2,14 @@ var controllers = angular.module("controllers", []);
 
 controllers.controller("HomeController", ['$scope', '$http', function ($scope, $http) {
     $scope.debug = false;
-   $scope.title = 'Hello ';
+    $scope.title = 'Hello ';
 
-   $http.get("service/home").success(function (data) {
-      $scope.data = data;
-      $scope.title += $scope.data.message;
-   });
+    $http.get("service/home").success(function (data) {
+        $scope.data = data;
+        $scope.title += $scope.data.message;
+    });
 
-   $scope.toggleDebug = function () {
-      $scope.debug = !$scope.debug;
-   };
+    $scope.toggleDebug = function () {
+        $scope.debug = !$scope.debug;
+    };
 }]);
